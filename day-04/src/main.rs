a61ae9bf5adee6355877b9facfbfd88d49c2b63d
Kevin Christen <chrkevin@amazon.com>
2023-12-14 09:22:34 -0800

    Day 4, part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -22,17 +22,22 @@ fn score_line(line: &str) -> usize {
         .collect();
     // println!("{:?}", yours);
     let num_winners = yours.into_iter().filter(|y| winners.contains(y)).count() as u32;
+
+    // part 1
     // println!("num_winners: {num_winners}");
-    if num_winners == 0 {
-        0
-    } else {
-        2_usize.pow(num_winners as u32 - 1)
-    }
+    // if num_winners == 0 {
+    //     0
+    // } else {
+    //     2_usize.pow(num_winners as u32 - 1)
+    // }
+
+    // part 2
+    num_winners.try_into().unwrap()
 }
 
 #[test]
 fn test_score_line() {
-    assert_eq!(16 /*16 90 33 7 46 */, score_line("Card   1: 98 16 95 90 53 33 43  7 46 45 | 85 15 78 57 34 10 46 90 33 13  8 54  4 37 25 63 55 41  7 82 69 16 30 76  2"));
+    assert_eq!(16, score_line("Card   1: 98 16 95 90 53 33 43  7 46 45 | 85 15 78 57 34 10 46 90 33 13  8 54  4 37 25 63 55 41  7 82 69 16 30 76  2"));
 }
 
 #[test]
@@ -45,10 +50,45 @@ fn test_single_score() {
     assert_eq!(1, score_line("Card  11: 1 | 1"))
 }
 
+fn get_copies(i: usize, orig: &Vec<(usize, usize)>) -> Vec<(usize, usize)> {
+    let mut res: Vec<(usize, usize)> = Vec::new();
+    let start = orig.get(i).unwrap();
+    for j in (i + 1)..(i + 1 + start.1) {
+        let cur = orig.get(j).unwrap();
+        res.push(*cur);
+        res.append(get_copies(j, orig).as_mut());
+    }
+    res
+}
+
+#[test]
+fn test_get_copies() {
+    let orig: Vec<(usize, usize)> = vec![(0, 2), (1, 1), (2, 0)];
+    let expected: Vec<(usize, usize)> = vec![(1, 1), (2, 0), (2, 0)];
+    assert_eq!(expected, get_copies(0, &orig));
+}
+
 fn main() {
-    let score = io::stdin()
+    // part 1
+    // let score = io::stdin()
+    //     .lines()
+    //     .map(|l| score_line(&l.unwrap()))
+    //     .fold(0, |acc, score| acc + score);
+    // println!("{score}");
+
+    // part 2
+    let originals: Vec<(usize, usize)> = io::stdin()
         .lines()
-        .map(|l| score_line(&l.unwrap()))
-        .fold(0, |acc, score| acc + score);
-    println!("{score}");
+        .enumerate()
+        .map(|(i, l)| (i, score_line(&l.unwrap())))
+        .collect();
+    println!("{:?}", originals);
+    let num_copies1 = originals
+        .iter()
+        .enumerate()
+        .map(|(i, c)| get_copies(i, &originals).len())
+        .reduce(|acc, count| acc + count);
+    println!("num_copies1 = {:?}", num_copies1);
+    // 6227972 is correct
+    println!("{}", originals.len() + num_copies1.unwrap());
 }
